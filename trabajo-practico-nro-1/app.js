94bfaa4bc065f658717ae888640250b2d0d76ac2
johannabinelli <johannabinelli@gmail.com>
2025-08-18 18:08:45 -0300

    modificaciones de acuerdo a correcciones del parcial

--- trabajo-practico-nro-1/app.js
+++ trabajo-practico-nro-1/app.js
@@ -1,3 +1,22 @@
 const inputMessage = document.querySelector(".text")
+const sendbutton = document.querySelector(".send")
+const chatMessages = document.querySelector(".chat-messages")
 
-console.log(inputMessage);
+sendbutton.addEventListener("click", sendMessage)
+
+
+function sendMessage() {
+  const message = inputMessage.value
+
+  const divMessage = documentcreateElement("div")
+
+  divMessage.classList.add("message-sent")
+
+  const text = document.createElement("p")
+  text.textContent = message
+
+  divMessage.appendChild(text)
+  chatMessages.appendChild(divMessage)
+
+  inputMessage.value = ""
+}
